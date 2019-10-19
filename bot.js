97881d792a2991a1dcf8d170d29b974015c8b7e3
PixelPerfeqt <43099607+PixelPerfeqt@users.noreply.github.com>
2019-10-19 01:39:21 -0400

    IT IS WEDNESDAY MY DUDES
    aaaaaAAAAAAA

--- bot.js
+++ bot.js
@@ -30,13 +30,12 @@ bot.on('messageCreate', async (msg) => {
            console.warn(err);
        }
    }
-});
 
-bot.on('message', msg => {
     if(msg.content.includes('Wednesday')){
-        msg.reply('https://youtu.be/du-TY1GUFGk');
+        msg.channel.createMessage('https://youtu.be/du-TY1GUFGk');
     }
 });
+
 
 bot.on('error', err => {
    console.warn(err);
